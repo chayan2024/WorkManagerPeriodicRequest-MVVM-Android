5e62b774483b56d8b1193f77c10ae94e19de44b7
chayan <chayanchowdhury@gmail.com>
2024-06-05 22:13:02 +0530

    implemented work manager with dagger hilt.

--- app/src/main/java/com/example/workmanagerperiodicrequest_mvvm_android/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/workmanagerperiodicrequest_mvvm_android/viewmodel/MainViewModel.kt
@@ -1,20 +1,17 @@
 package com.example.workmanagerperiodicrequest_mvvm_android.viewmodel
+import androidx.lifecycle.ViewModel
+import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.example.workmanagerperiodicrequest_mvvm_android.utils.PeriodicWorker
 import dagger.hilt.android.lifecycle.HiltViewModel
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
-import androidx.lifecycle.ViewModel
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.WorkManager
 
 @HiltViewModel
-class MainViewModel @Inject constructor(
-    private val workManager: WorkManager
-) : ViewModel() {
+class MainViewModel @Inject constructor(private val workManager: WorkManager) : ViewModel() {
 
     fun startPeriodicWork() {
-
         val periodicWorkRequest = PeriodicWorkRequestBuilder<PeriodicWorker>(10, TimeUnit.SECONDS)
             .build()
         workManager.enqueueUniquePeriodicWork(
